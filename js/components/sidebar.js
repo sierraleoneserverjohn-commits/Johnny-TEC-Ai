57703fe46bf4514270834b865017e837ede135ed
sierraleoneserverjohn-commits <sierraleoneserverjohn@gmail.com>
2026-08-09 05:12:37 +0000

    Update sidebar.js

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -1,1 +1,9 @@
+export function initSidebar() {
+    const sidebar = document.getElementById('left-sidebar');
+    const toggleBtn = document.getElementById('toggle-sidebar-btn');
+
+    toggleBtn.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+    });
+}
 
