54e4c593c0dbb49c3d3c09f8b17921041f0172c0
sierraleoneserverjohn-commits <sierraleoneserverjohn@gmail.com>
2026-08-09 05:13:17 +0000

    Update profile.js

--- js/components/profile.js
+++ js/components/profile.js
@@ -1,1 +1,11 @@
+export function initProfilePanel() {
+    const profilePanel = document.getElementById('right-profile-panel');
+    const profileIconBtn = document.getElementById('profile-icon-btn'); // You'll add this ID to the avatar
+
+    if(profileIconBtn) {
+        profileIconBtn.addEventListener('click', () => {
+            profilePanel.classList.toggle('active');
+        });
+    }
+}
 
